a1904b30835a5b7988b733d162d18de9c73a2ff0
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-11-22 17:32:11 +0000

    【Update】モデル作成

--- db/migrate/20221121030100_devise_create_users.rb
+++ db/migrate/20221121030100_devise_create_users.rb
@@ -32,7 +32,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :account_name
+      t.string :introduction
+      t.boolean :is_deleted, null: false, default: false
       t.timestamps null: false
     end
 
