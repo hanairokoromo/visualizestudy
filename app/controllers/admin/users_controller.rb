cacab934b9e26b2e59fd211a9a6cc0fe540bf6ed
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-11-23 09:54:36 +0000

    【Update】ユーザー情報画面の作成（管理者）

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,21 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_users_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :account_name, :email, :introduction, :is_deleted)
   end
 end
