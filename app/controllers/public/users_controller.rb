b773e770824190e160d75d30cabca20ed0d32044
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-11-24 00:39:31 +0000

    【Update】マイページの作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,30 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_paramas)
+    redirect_to user_path
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
+  def user_paramas
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :account_name, :introduction, :is_deleted)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   
-  namespace :admin do
-    get 'posts/show'
-  end
   root to: "public/homes#top"
   
   devise_for :users, skip:[:passwords], controllers:{
