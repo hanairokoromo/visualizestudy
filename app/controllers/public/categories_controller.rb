b8ae1d8e892fcf87da4fd4c1294a196fd4e4e992
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-11-27 14:59:37 +0000

    【Update】カテゴリー画面の作成

---
+++ app/controllers/public/categories_controller.rb
@@ -0,0 +1,32 @@
+class Public::CategoriesController < ApplicationController
+  def index
+    @category = Category.new
+    @categories =Category.all
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+        redirect_to categories_path
+    else
+        @categories = Category.all
+        render :index
+    end
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to categories_path
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:category_name)
+  end
+end
