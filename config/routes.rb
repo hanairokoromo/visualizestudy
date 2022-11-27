6583947755172c3b3856c1cdf3f9f71cdd6870e8
hanairokoromo <tneri.0mc5.rin2do2@gmail.com>
2022-11-27 14:59:37 +0000

    【Update】カテゴリー画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       end
     end
     resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :create, :edit, :update]
   end
   
   
