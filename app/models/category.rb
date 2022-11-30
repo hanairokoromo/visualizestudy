b7a86d50bac609755eaa1764ab34c94e62033de0
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-11-30 01:52:56 +0000

    【Fix】カテゴリー画面

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    belongs_to :users
+    belongs_to :user
     has_many :posts, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-    belongs_to :users
-    belongs_to :categories
+    belongs_to :user
+    belongs_to :category
     
     def favorited_by?(user)
         favorites.exsist?(user_id: user_id)
