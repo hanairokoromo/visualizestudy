5f294a65307f2584287924d93b77e7ede4e17550
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-12-31 02:54:48 +0000

    Update

--- db/migrate/20221222210006_add_comment_id_to_notifications.rb
+++ db/migrate/20221222210006_add_comment_id_to_notifications.rb
@@ -1,5 +1,5 @@
 class AddCommentIdToNotifications < ActiveRecord::Migration[6.1]
   def change
-    add_column :notifications, :comment_id, :integer
+    add_column :notifications, :comment_id, :integer, null: true
   end
 end
